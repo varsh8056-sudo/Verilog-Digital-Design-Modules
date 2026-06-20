0715014729a3b728f0a0c8f86cb7a735060246a0
varsh8056-sudo <varsh8056@gmail.com>
2026-06-21 00:21:03 +0530

    Create xor_gate.v

---
+++ xor_gate.v
@@ -0,0 +1,33 @@
+// Code your testbench here
+// or browse Examples
+module xorgate;
+  reg a1;
+  reg b1;
+  wire y;
+  xor_gate utp(.a(a1),.b(b1),.y(y));
+   initial begin
+    $dumpfile("dump.vcd");
+      $dumpvars(1);
+     
+ //   $monitor(a1,b1,y);
+    a1=1'b0;
+    b1=1'b0;
+   #1 $display("a1:%b, b1:%b, y1:%b",a1,b1,y);
+    #1
+    a1=1'b0;
+    b1=1'b1;
+     #1 $display("a1:%b, b1:%b, y1:%b",a1,b1,y);
+    #1
+    a1=1'b1;
+    b1=1'b0;
+     #1 $display("a1:%b, b1:%b, y1:%b",a1,b1,y);
+    #1
+    a1=1'b1;
+    b1=1'b1;
+end
+endmodule
+
+// Code your design here
+module xor_gate(input a,b,output y);
+  xor n1(y,a,b);
+endmodule
