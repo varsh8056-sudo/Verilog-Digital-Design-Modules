9d750f06a318e02a9d2b8205d4d2b614645b058a
varsh8056-sudo <varsh8056@gmail.com>
2026-06-22 22:44:54 +0530

    Create nand_as_not_gate.v

---
+++ nand_as_not_gate.v
@@ -0,0 +1,24 @@
+// testbench 
+module nano;
+  reg a1,b1;
+  wire y1;
+  NAasNO code(.a(a1),.b(b1),.y(y1));
+            integer i;
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,nano);
+    $monitor("TIME=%Ot ,INPUT VALUES a=%b, b=%b ,OUTPUT VALUE y=%b",$time,a1,b1,y1);
+    for(i=0;i<4;i=i+1) begin
+      b1=a1;
+      a1=i[1];
+      b1=i[0];
+      #4;
+    end
+  end
+endmodule
+
+
+//  design 
+module NAasNO(input a,b,output y);
+  nand(y,a,b);
+endmodule
